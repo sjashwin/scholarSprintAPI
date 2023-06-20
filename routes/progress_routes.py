9f4f843a637e075d33fc1a55cc5d1778e61fc73b
Ashwin Thinnappan <ashwin@codseg.com>
2023-06-20 16:49:50 +0530

    Added Progress Route

--- routes/progress_routes.py
+++ routes/progress_routes.py
@@ -1,7 +1,28 @@
-from mongo.mongo import QUIZ_COLLECTION, QUESTION_COLLECTION
+from mongo.mongo import QUIZ_COLLECTION, QUESTION_COLLECTION, PROGRESS_COLLECTION
+from mongo.PyObjectId import PyObjectId
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
 router = APIRouter()
+
+class Progress(BaseModel):
+    t: str
+    d: str
+    qID: str = ""
+    quID: List[str] = []
+    r: int = 0
+
+class Progress(BaseModel):
+    id: Optional[PyObjectId] = Field(default_factor=PyObjectId, alias='_id')
+    uid: str
+    score: int
+    progress: List[Progress] = []
+    
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {PyObjectId: str}
 
 @router.get("/quizCount")
 async def quizCount():
@@ -12,3 +33,14 @@ async def quizCount():
 async def questionCount():
     count = await QUESTION_COLLECTION.count_documents({})
     return {"total": count}
+
+@router.get("/progress")
+async def read_progress(userID: Optional[str] = None):
+    if userID is None:
+        doc = await PROGRESS_COLLECTION.find({}).to_list(None)
+        for progress in doc:
+            progress["_id"] = str(progress["_id"])
+    else:
+        doc = await PROGRESS_COLLECTION.find_one({"uid": userID})
+        doc["_id"] = str(doc["_id"])
+    return {"user": doc}
