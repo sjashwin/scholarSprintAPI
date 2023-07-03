330cdde1c50f8defabcd5d58dc3d6cb1a4b55752
Ashwin Thinnappan <ashwin@codseg.com>
2023-07-03 15:13:17 +0530

    Parallel Fetch All Domains For Dashboard

--- routes/quiz_routes.py
+++ routes/quiz_routes.py
@@ -3,24 +3,26 @@ from typing import List
 from models.quiz import Quiz
 from mongo.mongo import QUIZ_COLLECTION
 from typing import Optional
+import logging
+import asyncio
 
 router = APIRouter()
 
-async def fetch_data(query, collection, n):
-    pipeline = [{'$match': query}, {'$sample': {'size': n}}]
-    async for doc in collection.aggregate(pipeline):
-        yield doc
+async def fetch_quiz_by_domain(domain: int, n: int):
+    pipeline = [{'$match': {'domain.0': domain}}, {'$sample': {'size': n}}]
+    doc = await QUIZ_COLLECTION.aggregate(pipeline).to_list(n)
+    return doc
 
 async def get_quiz_from_db(n: int = 10):
     quiz = []
-    for i in range(1, 9):
-        pipeline = [{'$match': {"domain.0": i}}, {'$sample': {'size': 10}}]
-        doc = await QUIZ_COLLECTION.aggregate(pipeline).to_list(n)
-        quiz.extend(doc)
-    pipeline = [{'$match': {"domain.2": 8}}, {'$sample': {'size': 10}}]
+    domains = list(range(1, 9))
+    tasks = [fetch_quiz_by_domain(domain, n) for domain in domains]
+    quiz_results = await asyncio.gather(*tasks)
+    quiz.extend([item for sublist in quiz_results for item in sublist])
+    pipeline = [{'$match': {"domain.2": 8}}, {'$sample': {'size': n}}]
     doc = await QUIZ_COLLECTION.aggregate(pipeline).to_list(n)
     quiz.extend(doc)
-    pipeline = [{'$match': {"domain.2": 9}}, {'$sample': {'size': 10}}]
+    pipeline = [{'$match': {"domain.2": 9}}, {'$sample': {'size': n}}]
     doc = await QUIZ_COLLECTION.aggregate(pipeline).to_list(n)
     quiz.extend(doc)
 
@@ -28,8 +30,9 @@ async def get_quiz_from_db(n: int = 10):
 
 @router.get("/createquiz", response_model=List[Quiz], status_code=200)
 async def create_quiz(data: Optional[dict] = {}):
-    size = data.get("size")
-    return await get_quiz_from_db(size)
+    size = data.get("size") or 10
+    logging.debug(f"Fetching Quizzes")
+    return await get_quiz_from_db(int(size))
 
 @router.get("/preps", response_model=List[Quiz], status_code=200)
 async def preps():
